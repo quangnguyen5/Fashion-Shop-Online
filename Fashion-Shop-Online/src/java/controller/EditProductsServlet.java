1a086c0df2dc36900f3e3b649aac29b7bab8dc16
quangnguyen5 <quangnguyen20082002@gmail.com>
2023-02-10 08:08:17 +0700

    update product page, product manage page

---
+++ Fashion-Shop-Online/src/java/controller/EditProductsServlet.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dal.ProductDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.Product;
+import model.Product_Test;
+
+/**
+ *
+ * @author LEGION 5
+ */
+public class EditProductsServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDAO dao = new ProductDAO();
+        String index = req.getParameter("index");
+        if (index == null) {
+            index = "1";
+        }
+        int indexPage = Integer.parseInt(index);
+        ArrayList<Product> listProduct = dao.getListProductPage(indexPage);
+        
+        int totalProduct = dao.getTotalProducts();
+        int endPage  = totalProduct/7;
+        if (totalProduct % 7 != 0){
+            endPage++;
+        }
+        System.out.println(listProduct.get(0).getName());
+        req.setAttribute("endPage", endPage);
+        req.setAttribute("totalProduct",totalProduct);
+        req.setAttribute("listProduct", listProduct);
+        req.getRequestDispatcher("edit_products.jsp").forward(req, resp);
+    }
+
+}
