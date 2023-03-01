690f26a3ed026fc85846ce4c041671e4530accaf
Trmingnw <minhnthe161666@fpt.edu.vn>
2023-03-01 23:31:52 +0700

    Update BlogController.java

--- Fashion-Shop-Online/src/java/controller/BlogController.java
+++ Fashion-Shop-Online/src/java/controller/BlogController.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.Blog;
-
+import model.CategoryBlog;
 
 
 @WebServlet(name = "BlogController", urlPatterns = {"/blog"})
